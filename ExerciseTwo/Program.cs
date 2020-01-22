a7e403664c6170518e58640dd9c68fc61fb2e074
Divyang Mehta <dmehta@mail.usf.edu>
2020-01-21 20:56:21 -0500

    Add project files.

---
+++ ExerciseTwo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExerciseTwo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int age = 23;
+			int height = 70;
+			Console.WriteLine("Age = " + age + '\n' + "Height = " + height);
+			Console.WriteLine("I am " + age + " years old, my max heart rate is " + (220 - age) + " and I am " + height + " inches tall.");
+		}
+	}
+}
